45714b497103de2429f618d1769f31128c7d8e76
Anton Trollbäck <anton@trollback.se>
2014-02-06 16:16:00 -0500

    Add some js for selecting output

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,34 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+
+(function (window, document) {
+
+  // Rocument ready
+  document.addEventListener('DOMContentLoaded', function() {
+
+    var output = document.querySelectorAll('.output')[0];
+    var hint = document.querySelectorAll('.hint')[0];
+
+    function selectOutput() {
+      output.focus();
+      output.select();
+
+      // For iOS
+      output.selectionStart = 0;
+      output.selectionEnd = output.value.length;
+    };
+
+    function copyOutput() {
+      alert('we should add the copy code here')
+    };
+
+    output.addEventListener('click', selectOutput);
+    output.addEventListener('touchstart', selectOutput);
+    hint.addEventListener('click', copyOutput);
+    hint.addEventListener('touchstart', copyOutput);
+
+  });
+
+})(window, document);
