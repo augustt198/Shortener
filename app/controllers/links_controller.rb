dd4bd4b4d598d7c39c59fd8dc329ca5e52bb0dae
August <augustt198@gmail.com>
2014-02-05 21:43:25 -0500

    URL checks & message flashes

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,8 +2,19 @@ class LinksController < ApplicationController
 
   require 'digest/md5'
 
-
   def create
+    begin
+      uri = URI.parse(params[:link][:link])
+    rescue URI::InvalidURIError
+      flash[:error] = "Invalid URL"
+      redirect_to '/'
+      return
+    end
+    if uri.class != URI::HTTP or uri.class != URI::HTTPS
+      flash[:error] = "Invalid URL"
+      redirect_to '/'
+      return
+    end
     while true do
       urlhash = Digest::MD5.hexdigest(Time.now.to_s).slice(0, 4)
       if Link.where(urlhash: urlhash).first == nil
