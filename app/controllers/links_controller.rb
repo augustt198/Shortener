9e99d01bde4d30ddd1dd9dd52df74da1c7ec47f9
August <augustt198@gmail.com>
2014-02-06 18:04:57 -0500

    Better link validating

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,8 +3,14 @@ class LinksController < ApplicationController
   require 'digest/md5'
 
   def create
+    url = params[:link][:link]
+    if url.start_with?('www')
+      url = "http://" + url
+    elsif !url.downcase.start_with?('http') and !url.downcase.start_with?('http')
+      url = "http://" + url
+    end
     begin
-      uri = URI.parse(params[:link][:link])
+      uri = URI.parse(url)
     rescue URI::InvalidURIError
       flash[:error] = "Invalid URL"
       redirect_to '/'
@@ -15,11 +21,16 @@ class LinksController < ApplicationController
       redirect_to '/'
       return
     end
+    if uri.host.split('.').length < 2
+      flash[:error] = "Invalid URL"
+      redirect_to '/'
+      return
+    end
     while true do
       urlhash = Digest::MD5.hexdigest(Time.now.to_s).slice(0, 4)
       if Link.where(urlhash: urlhash).first == nil
         link = Link.create
-        link.link = params[:link][:link]
+        link.link = url
         link.urlhash = urlhash
         link.hits = 0
         link.save!
